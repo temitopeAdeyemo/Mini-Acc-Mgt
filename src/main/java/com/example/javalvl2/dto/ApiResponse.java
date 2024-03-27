39f22210958310db563bcd65210363db7b882a3f
Temitope Adeyemo <apple@Temitope.local>
2024-03-27 23:11:34 +0100

    Logged

--- src/main/java/com/example/javalvl2/dto/ApiResponse.java
+++ src/main/java/com/example/javalvl2/dto/ApiResponse.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.http.HttpStatus;
 
 @JsonIgnoreProperties(ignoreUnknown = true) // unknown properties that is not in the class properties would be ignored in the response without throwing an err
-//@JsonInclude(JsonInclude.Include.NON_NULL) // Will exclude properties with null values
+//@JsonInclude(JsonInclude.Include.NON_NULL) // Will exclude properties with null values.
 public class ApiResponse<T> {
     @JsonProperty("success")
     private boolean success;
